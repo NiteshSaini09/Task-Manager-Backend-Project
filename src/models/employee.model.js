7f25e403dcb5f130f306340fc97195c17b8d3640
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-08-28 22:12:28 +0530

    Update employee.model.js
    fields filter

--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -1,7 +1,19 @@
 import mongoose from "mongoose";
 const employeeSchema=mongoose.Schema({
-      name:{},
-      age:{}
+      name:{
+        type:String,
+        required:true
+},
+      age:{
+        type:Number,
+        required:true,
+        min(1),
+        max(120)
+},
+      user:{
+        type:mongoose.Types.Schema.ObjectId,
+        ref:"User"
+}
 },{timestamps:true})
 
 export const EmployeeModel=mongoose.model("Employee", employeeSchema)
